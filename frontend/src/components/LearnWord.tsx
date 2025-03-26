755378242461f39a8868a28c99ffd83d21238fbf
Kayo Osako <osakok@cardiff.ac.uk>
2025-03-26 01:16:41 +0000

    delete debug sentences

--- frontend/src/components/LearnWord.tsx
+++ frontend/src/components/LearnWord.tsx
@@ -3,15 +3,15 @@ import { learnWord } from '../api';
 
 
 interface LearnWordProps {
-    recognizedWord: string;  // SpeechRecognitionComponent から受け取る
+    recognizedWord: string;  // receive word from SpeechRecognitionComponent
 }
 
 const LearnWord: React.FC<LearnWordProps> = ({ recognizedWord }) => {
 
     const handleLearnWord = async () => {
         if (recognizedWord) {
-            await learnWord(recognizedWord); // API 呼び出し
-            alert("Word Learned: " + recognizedWord);
+            await learnWord(recognizedWord); // API call
+            //alert("Word Learned: " + recognizedWord);
         }
     };
 
