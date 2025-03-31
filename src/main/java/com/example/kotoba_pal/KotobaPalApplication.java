69c5d65654ccb5a0abd569f0dd7ac1dc2eec96db
Kayo Osako <osakok@cardiff.ac.uk>
2025-03-31 05:55:31 +0100

    a

--- src/main/java/com/example/kotoba_pal/KotobaPalApplication.java
+++ src/main/java/com/example/kotoba_pal/KotobaPalApplication.java
@@ -2,8 +2,11 @@ package com.example.kotoba_pal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.kotoba_pal.config.WebConfig;  // WebConfigクラスをインポート
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfig.class)  // CORS設定を明示的にインポート
 public class KotobaPalApplication {
 
 	public static void main(String[] args) {
