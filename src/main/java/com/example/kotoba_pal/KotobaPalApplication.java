553001f40ec876eb9f4b0dba285f8bde99630bc6
Kayo Osako <osakok@cardiff.ac.uk>
2025-03-31 09:58:00 +0100

    a

--- src/main/java/com/example/kotoba_pal/KotobaPalApplication.java
+++ src/main/java/com/example/kotoba_pal/KotobaPalApplication.java
@@ -3,11 +3,10 @@ package com.example.kotoba_pal;
 import com.example.kotoba_pal.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.kotoba_pal.config.WebConfig;  // WebConfigクラスをインポート
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebConfig.class, SecurityConfig.class}) // CORSとセキュリティ設定を明示的にインポート
+@Import({SecurityConfig.class}) // CORSとセキュリティ設定を明示的にインポート
 public class KotobaPalApplication {
 
 	public static void main(String[] args) {
